7198049fddbc4a8c9d3b735f17c304ccb31f0331
Jodecir Neto <jodecirneto@gmail.com>
2021-09-28 16:14:19 -0300

    CREATE convert button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-//entrada
-var c = prompt("Qual é a temperatura em c");
-var f = prompt("Qual é a temperatura em c");
-//processamento
-var ftoc = (c * 1.8) + 32;
-var ctof = (f - 32) / 1.8;
-//saída
-alert("A temp. em C é " + ctof + "°");
-alert("A temperatura em F é " + ftoc + "°");
+function ConvertInput() {
+  //entrada
+  var c = parseInt(document.getElementById("c").value);
+  var f = parseInt(document.getElementById("f").value);
+  //processamento
+  var ftoc = (c * 1.8) + 32;
+  var ctof = (f - 32) / 1.8;
+  //saída
+  alert("A temperatura em C é " + ctof + "°");
+  alert("A temperatura em F é " + ftoc + "°");
+}
