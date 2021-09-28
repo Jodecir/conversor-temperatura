f3c9a79f7436ff3aad5def3b3767d11ac02687c3
Jodecir Neto <jodecirneto@gmail.com>
2021-09-28 15:51:31 -0300

    UPDATE convertor in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //entrada
 var c = prompt("Qual é a temperatura em c");
+var f = prompt("Qual é a temperatura em c");
 //processamento
-var f = (c * 1.8) + 32;
+var ftoc = (c * 1.8) + 32;
+var ctof = (f - 32) / 1.8;
 //saída
-alert("A temperatura em F é " + f + "°");
+alert("A temp. em C é " + ctof + "°");
+alert("A temperatura em F é " + ftoc + "°");
