068663909f178e7186eb7aaba6284846893bd15a
Jodecir Neto <jodecirneto@gmail.com>
2022-01-25 13:02:18 -0300

    UPDATE index with script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function ConvertInput() {
   var ftoc = (c * 1.8) + 32;
   var ctof = (f - 32) / 1.8;
   //saída
-  alert("A temperatura em C é " + ctof + "°");
-  alert("A temperatura em F é " + ftoc + "°");
+  document.getElementById('c').value = ctof
+  document.getElementById('f').value = ftoc
 }
